64427dc5dcf47149e22aa883055148dfa35f1b48
Manu <jmgonzalez@hola.com>
2019-01-02 10:18:01 +0100

    Form Handling and CSRF Protection

---
+++ resources/views/projects/create.blade.php
@@ -0,0 +1,23 @@
+@extends('layout')
+
+@section('title')
+    Create a New Project
+@endsection
+
+@section('content')
+    <h1>Create a New Project</h1>
+
+    <form method="POST" action="/projects">
+        {{ csrf_field() }}
+        <div>
+            <input type="text" name="title" placeholder="Project title">
+        </div>
+        <div>
+            <textarea name="description" placeholder="Project description"></textarea>
+        </div>
+
+        <div>
+            <button type="submit">Create Project</button>
+        </div>
+    </form>
+@endsection
