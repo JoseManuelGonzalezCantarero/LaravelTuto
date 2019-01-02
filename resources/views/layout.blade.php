44fe45d1d712b54ef1a75746845519049490c59a
Manu <jmgonzalez@hola.com>
2019-01-02 13:03:03 +0100

    Faking PATCH and DELETE requests

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,14 +3,17 @@
 <html>
     <head>
         <title>@yield('title', 'Laracasts')</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.css">
     </head>
     <body>
-        <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/about">About Us</a></li>
-            <li><a href="/contact">Contact</a> us to learn more.</li>
-        </ul>
-
-        @yield('content')
+        <div class="container">
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/about">About Us</a></li>
+                <li><a href="/contact">Contact</a> us to learn more.</li>
+            </ul>
+            <br>
+            @yield('content')
+        </div>
     </body>
 </html>
