de02901fc618341ebe6ffaed1dbfcc3668c2316e
Manu <jgonzalezcantarero@gmail.com>
2019-01-08 11:43:41 +0100

    When in doubt

--- app/Http/Controllers/ProjectTasksController.php
+++ app/Http/Controllers/ProjectTasksController.php
@@ -3,19 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Project;
-use App\Task;
 
 class ProjectTasksController extends Controller
 {
-    public function update(Task $task)
-    {
-        $method = request()->has('completed') ? 'complete' : 'incomplete';
-
-        $task->$method();
-
-        return back();
-    }
-
     public function store(Project $project)
     {
         $attributes = request()->validate([
